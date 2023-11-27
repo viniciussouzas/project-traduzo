dd8e7eaecf92d6a12afb31f2db2f21c33e782944
Vinícius Souza <tradutorvinicius@gmail.com>
2023-11-27 13:14:51 -0300

    Resolve requisito 2 e 3

---
+++ src/models/language_model.py
@@ -0,0 +1,15 @@
+from database.db import db
+from models.abstract_model import AbstractModel
+
+
+class LanguageModel(AbstractModel):
+  _collection = db["languages"]
+
+  def __init__(self, json_data: dict):
+    super().__init__(json_data)
+
+  def to_dict(self):
+    return {
+      "name": self.data["name"],
+      "acronym": self.data["acronym"],
+    }
