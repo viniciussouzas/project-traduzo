7f4e4533c8aecacc0d7ddf9ca3841616ae7be776
Vinícius Souza <tradutorvinicius@gmail.com>
2023-11-27 13:28:12 -0300

    fix: lint again

--- src/models/language_model.py
+++ src/models/language_model.py
@@ -6,16 +6,15 @@ class LanguageModel(AbstractModel):
     _collection = db["languages"]
 
     def __init__(self, json_data: dict):
-      super().__init__(json_data)
+        super().__init__(json_data)
 
     def to_dict(self):
-      return {
-        "name": self.data["name"],
-        "acronym": self.data["acronym"],
+        return {
+            "name": self.data["name"],
+            "acronym": self.data["acronym"],
       }
-
 
     @classmethod
     def list_dicts(cls):
-      data = cls.find()
-      return [cls.to_dict(d) for d in data]
+        data = cls.find()
+        return [cls.to_dict(d) for d in data]
